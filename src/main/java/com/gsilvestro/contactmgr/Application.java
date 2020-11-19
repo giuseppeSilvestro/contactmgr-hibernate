6c5504a1fe5ed72d06236c73e9376e3e59a64a58
Giuseppe Silvestro <giuseppe.silvestro@live.com>
2020-11-19 16:33:08 +0000

    In the main implement the code to begin a transaction and commit it to the database. I had to add some dependencies in the build.gradle because some libraries are not included anymore in JavaSE. I also had to change the Hibernate dependency and use a newer one because I was getting some errors due with the interaction with H2Dialect.

--- src/main/java/com/gsilvestro/contactmgr/Application.java
+++ src/main/java/com/gsilvestro/contactmgr/Application.java
@@ -1,6 +1,7 @@
 package com.gsilvestro.contactmgr;
 
 import com.gsilvestro.contactmgr.model.Contact;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
@@ -9,7 +10,7 @@ import org.hibernate.service.ServiceRegistry;
 
 public class Application {
     // Hold a reusable reference to a SessionFactory (since we need only one)
-    //private static final SessionFactory sessionFactory = buildSessionFactory();
+    private static final SessionFactory sessionFactory = buildSessionFactory();
 
     private static SessionFactory buildSessionFactory() {
         // Create a StandardServiceRegistry
@@ -24,6 +25,22 @@ public class Application {
                 .withPhone(123456789L)
                 .build();
 
-        System.out.println(contact);
+        // Open a session
+        Session session = sessionFactory.openSession();
+
+        // Begin a transaction
+        // To support Hibernate transaction, we need to add a JTA dependency to our build.gradle
+        session.beginTransaction();
+
+        // Use the session to save the contact
+        session.save(contact);
+
+        // Commit the transaction
+        // In the hibernate.cfg.xml we have added a property 'show_sql' and set it to 'true'
+        // so that every time a transaction is executed we have a message.
+        session.getTransaction().commit();
+
+        // Close the session
+        session.close();
     }
 }
--- src/main/java/com/gsilvestro/contactmgr/model/Contact.java
+++ src/main/java/com/gsilvestro/contactmgr/model/Contact.java
@@ -11,12 +11,16 @@ public class Contact {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     @Column
     private String firstName;
+
     @Column
     private String lastName;
+
     @Column
     private String email;
+
     @Column
     private Long phone;
 
