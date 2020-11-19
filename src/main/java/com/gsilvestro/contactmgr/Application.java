a8755723f755de933b7ff500e349f28d9f68ffa2
Giuseppe Silvestro <giuseppe.silvestro@live.com>
2020-11-19 14:01:35 +0000

    Add  SessionFactory in the Application.java. (Now it is commented out because we do not need all its functions yet)
    Introduce the builder pattern to add our Contact objects.

--- src/main/java/com/gsilvestro/contactmgr/Application.java
+++ src/main/java/com/gsilvestro/contactmgr/Application.java
@@ -1,7 +1,29 @@
 package com.gsilvestro.contactmgr;
 
+import com.gsilvestro.contactmgr.model.Contact;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+
+
 public class Application {
+    // Hold a reusable reference to a SessionFactory (since we need only one)
+    //private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        // Create a StandardServiceRegistry
+        final ServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        return new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    }
+
     public static void main(String[] args) {
 
+        Contact contact = new Contact.ContactBuilder("Giuseppe", "Silvestro")
+                .withEmail("cnslanc@live.com")
+                .withPhone(123456789L)
+                .build();
+
+        System.out.println(contact);
     }
 }
